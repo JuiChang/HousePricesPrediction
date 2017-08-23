a96fa2eb676aa3fc84d8e26431fc2ed81ca3225b
Rey-Chang <drjuichang@gmail.com>
2017-08-23 14:21:09 +0800

    random forest

--- HousePricesPrediction_Rscript.R
+++ HousePricesPrediction_Rscript.R
@@ -1,8 +1,10 @@
+#cv reference: https://www.analyticsvidhya.com/blog/2015/11/improve-model-performance-cross-validation-in-python-r/
 library("Matrix")
 library("xgboost")
 library("dplyr")
 library("DMwR")
 library("hydroGOF")
+library('randomForest')
 
 train <- read.csv('../input/House-Prices/train.csv', stringsAsFactors = T)
 test <- read.csv('../input/House-Prices/test.csv', stringsAsFactors = T)
@@ -22,78 +24,38 @@ test2 <- test[colnames(test) %in% colnames(train_rm_col_200_2)]
 #Imputation for the test data
 imputed_test2 <- knnImputation(test2) 
 
-
-#remove the observations of training data which include NA
-train_rm_na_obser <- train_rm_col_200[complete.cases(train_rm_col_200), ]
-
-train_price <- data.frame(Id=train_rm_na_obser$SalePrice,
-                          Price=train_rm_na_obser$SalePrice)
-train_rm_na_obser_noPrice <- train_rm_na_obser
-train_rm_na_obser_noPrice$SalePrice <- NULL
+#Imputation for the train data
+train_rm_na_obser <- knnImputation(train_rm_col_200)
 
 #THE DATA: train_price   train_rm_na_obser_noPrice   imputed_test2
 
-#the variables of the two sparse matrices(from train and test) was different
-#so I merge the two data frame and convert to a sparse matrix, then split it back
-#to train part and test part.
-#df_all <- rbind(train_rm_na_obser_noPrice, imputed_test2)
-#df_all_sparse_matrix <- sparse.model.matrix(~., data=df_all)
-#train_sparse_matrix_2 <- df_all_sparse_matrix[train_rm_na_obser$Id,]
-#test_sparse_matrix_2 <- df_all_sparse_matrix[
-#                            !((df_all$Id)%in%(train_rm_na_obser$Id)),]
 
 
 k_fold <- 5
-train_rm_na_obser_noPrice$Id <- sample(1:k_fold, 
+train_rm_na_obser$Id <- sample(1:k_fold, 
                                        nrow(train_rm_na_obser_noPrice), 
                                        replace = TRUE)
-
-train_price$Id <- train_rm_na_obser_noPrice$Id
 
 prediction <- data.frame()
 testsetCopy <- data.frame()
 
 for(i in 1:k_fold){
     #useful in other place
-    #train_set <- train_rm_na_obser_noPrice[!(train_rm_na_obser_noPrice$Id==i),]
-    #test_set <- train_rm_na_obser_noPrice[train_rm_na_obser_noPrice$Id==i,]
+    train_set <- train_rm_na_obser[!(train_rm_na_obser$Id==i),]
+    test_set <- train_rm_na_obser[train_rm_na_obser$Id==i,]
+    test_set$SalePrice <- NULL
     
-    #for xgboost specifically
-    #df_all <- rbind(train_set, test_set)
-    df_all_sparse_matrix <- sparse.model.matrix(~., data=train_rm_na_obser_noPrice)
-    train_sparse_matrix_2 <- df_all_sparse_matrix[
-        !(train_rm_na_obser_noPrice$Id==i),]
-    test_sparse_matrix_2 <- df_all_sparse_matrix[
-        train_rm_na_obser_noPrice$Id==i,]
-    
-    #form the model
-    xgb <- xgboost(data = data.matrix(train_sparse_matrix_2), 
-                   label = train_price$Price[train_price$Id!=i],
-                   #booster="gblinear", #default is "gbtree"
-                   objective="reg:linear",
-                   nround=25)
+    rf_model <- randomForest(SalePrice ~ .,
+                             data = train_set)
     
     #predict
-    temp <- as.data.frame(predict(xgb, test_sparse_matrix_2))
+    temp <- as.data.frame(predict(rf_model, test_set))
     prediction <- rbind(prediction, temp)
     
     #order the label of test data
-    temp2 <- as.data.frame(train_price$Price[train_price$Id==i])
+    temp2 <- as.data.frame(train_rm_na_obser$SalePrice[train_rm_na_obser$Id==i])
     testsetCopy <- rbind(testsetCopy, temp2)
     
     #error
     err <- mse(prediction, testsetCopy)
 }
-
-
-#form the model
-#xgb <- xgboost(data = data.matrix(train_sparse_matrix_2), 
-#               label = train_price,
-#               objective="reg:linear",
-#               nround=25)
-
-#predict
-#y_pred <- predict(xgb, test_sparse_matrix_2)
-
-#y_pred_df <- data.frame(y_pred)
-#write.csv(y_pred_df, "submission_2.csv")
